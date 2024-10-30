901078d7504c0e07bc7eeb94679b1273c1a57540
Dinh Quan <quanvonguyendinh@gmail.com>
2024-10-30 21:10:09 +0700

    config css sass

--- src/MyAlear.js
+++
@@ -1,5 +0,0 @@
-const myAlert = () => {
-  alert("wassup");
-};
-
-export default myAlert;
--- src/components/Button/index.js
+++
@@ -1,7 +0,0 @@
-import myAlert from "~/MyAlear";
-
-function Button() {
-  return <button onClick={myAlert}>Click Me</button>;
-}
-
-export default Button;
