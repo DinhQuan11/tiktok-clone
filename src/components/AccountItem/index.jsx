76358a9de4b61ad242cf5ba072ee9c7e05ed3267
Dinh Quan <quanvonguyendinh@gmail.com>
2024-11-06 12:06:01 +0700

    handle logic search

--- src/components/AccountItem/index.jsx
+++ src/components/AccountItem/index.jsx
@@ -1,6 +1,7 @@
 import classNames from "classnames/bind";
 
 import styles from "./AccountItem.module.scss";
+import Image from "~/components/Image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 
@@ -9,7 +10,7 @@ const cx = classNames.bind(styles);
 function AccountItem() {
   return (
     <div className={cx("wrapper")}>
-      <img
+      <Image
         className={cx("avatar")}
         src="https://lh3.googleusercontent.com/a/ACg8ocLr-WaQOOYYgxufAz56i6lS4c5fEgjmV_zPsfmVuha2wuYxZu3H=s360-c-no"
         alt="DinhQuanELV"
--- src/components/Layout/components/Header/index.jsx
+++ src/components/Layout/components/Header/index.jsx
@@ -1,18 +1,13 @@
-import { useEffect, useState } from "react";
 import classNames from "classnames/bind";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Tippy from "@tippyjs/react";
-import HeadlessTippy from "@tippyjs/react/headless";
 import "tippy.js/dist/tippy.css";
 import {
   faArrowRightFromBracket,
-  faCircleXmark,
   faCoins,
   faEarthAsia,
   faEllipsisVertical,
   faGear,
-  faMagnifyingGlass,
-  faSpinner,
 } from "@fortawesome/free-solid-svg-icons";
 import {
   faCircleQuestion,
@@ -25,10 +20,9 @@ import Button from "~/components/Button";
 import styles from "./Header.module.scss";
 import images from "~/assets/images";
 import Image from "~/components/Image";
-import { Wrapper as PopperWrapper } from "~/components/Popper";
-import AccountItem from "~/components/AccountItem";
 import Menu from "~/components/Popper/Menu";
 import { MessageIcon, UploadIcon } from "~/components/Icons";
+import Search from "../Search";
 
 const cx = classNames.bind(styles);
 
@@ -64,15 +58,7 @@ const MENU_ITEMS = [
 ];
 
 function Header() {
-  const [searchResult, setSearchResult] = useState([]);
-
   const currentUser = true;
-
-  useEffect(() => {
-    setTimeout(() => {
-      setSearchResult([]);
-    }, 0);
-  }, []);
 
   // Handle logic
   const handleMenuChange = (menuItem) => {
@@ -114,37 +100,8 @@ function Header() {
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
         <img src={images.logo} alt="Tiktok" />
-        <HeadlessTippy
-          interactive
-          visible={searchResult.length > 0}
-          render={(attrs) => (
-            <div className={cx("search-result")} tabIndex="-1" {...attrs}>
-              <PopperWrapper>
-                <h4 className={cx("search-title")}>Accounts</h4>
-                <AccountItem />
-                <AccountItem />
-                <AccountItem />
-                <AccountItem />
-                <AccountItem />
-              </PopperWrapper>
-            </div>
-          )}
-        >
-          <div className={cx("search")}>
-            <input
-              placeholder="Search accounts and videos"
-              spellCheck={false}
-            />
-            <button className={cx("clear-btn")}>
-              {<FontAwesomeIcon icon={faCircleXmark} />}
-            </button>
-            <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
 
-            <button className={cx("search-btn")}>
-              {<FontAwesomeIcon icon={faMagnifyingGlass} />}
-            </button>
-          </div>
-        </HeadlessTippy>
+        <Search />
 
         <div className={cx("actions")}>
           {currentUser ? (
