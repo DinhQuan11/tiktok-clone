e180d7f90cdae67a0d13888458884feaff6e9da2
Dinh Quan <quanvonguyendinh@gmail.com>
2024-11-07 11:48:40 +0700

    config routes

--- src/components/Layout/components/Header/index.jsx
+++ src/components/Layout/components/Header/index.jsx
@@ -2,6 +2,7 @@ import classNames from "classnames/bind";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Tippy from "@tippyjs/react";
 import "tippy.js/dist/tippy.css";
+import { Link } from "react-router-dom";
 import {
   faArrowRightFromBracket,
   faCoins,
@@ -16,6 +17,7 @@ import {
   faUser,
 } from "@fortawesome/free-regular-svg-icons";
 
+import routesConfig from "~/config/routes";
 import Button from "~/components/Button";
 import styles from "./Header.module.scss";
 import images from "~/assets/images";
@@ -99,7 +101,9 @@ function Header() {
   return (
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
-        <img src={images.logo} alt="Tiktok" />
+        <Link to={routesConfig.home} className={cx("logo-link")}>
+          <img src={images.logo} alt="Tiktok" />
+        </Link>
 
         <Search />
 
